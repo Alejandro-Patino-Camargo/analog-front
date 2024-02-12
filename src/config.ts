708b6b0b198a8cf01b7ec488c6cb0cae140139fd
Alejandro Patino Camargo <alejandropatinoc@gmail.com>
2024-02-12 10:29:56 -0800

    backend connection bug

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ let base_url: string;
 if (import.meta.env.NODE_ENV === "production") {
   base_url = import.meta.env.VITE_REACT_APP_API_URI as string;
 } else {
-  base_url = "http://localhost:8000";
+  base_url = "https://analog-backend.onrender.com";
 }
 console.log("base_url", import.meta.env.VITE_NODE_ENV);
 
