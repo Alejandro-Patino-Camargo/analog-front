af3cf3d9f6388e2e5409ec380b929ff581b4bdb3
Alejandro Patino Camargo <alejandropatinoc@gmail.com>
2024-02-11 21:28:58 -0800

    updated env process

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,10 @@
 let base_url: string;
 
-if (import.meta.env.MODE === "production") {
+if (import.meta.env.NODE_ENV === "production") {
   base_url = import.meta.env.VITE_REACT_APP_API_URI as string;
 } else {
   base_url = "http://localhost:8000";
 }
+console.log("base_url", import.meta.env.VITE_NODE_ENV);
 
 export { base_url };
